db900cb044c06c274109f0401d72d82af5d01643
DreamsInAutumn <99279454+DreamsInAutumn@users.noreply.github.com>
2026-06-18 04:15:35 +0000

    Update uniloader2.cmd for new features and fixes
    removed some powershell dependencies
    added an inifile parser

--- Examples/uniloader2.cmd
+++ Examples/uniloader2.cmd
@@ -37,72 +37,31 @@
 	%RTS%
 }
 
+
 :: -- Initialization --
 
 :InitNameSpace {
-	:: -- User Config
-		set "uldr.browserDelay=3"					:: Delay before browser starts, allows user to read our console messages breifly.
-		set "uldr.appDelay=6"						:: Delay before exiting
-		set "uldr.maxTriesHTTP=60"					:: Number of retrie to check if the HTTP Server is CheckServerRunning
-		set "uldr.debug=False"						:: Pauses at the end, and disables the Browser Launch
-		set "uldr.minMaxTerminal.state=void"		:: options: Disabled / Void
-
-	:: -- App List
-		set "uldr.app[0]=ComfyUI_3"
-		set "uldr.app[1]=SillyTavern"
-
 	:: Internal Config
-		set "uldr.appver=v2.04a"		
+		set "uldr.appver=v2.10a"
 		set "uldr.appPath=%~dp0"
 		set "uldr.powerShellExe=C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
 
+	:: Ini Parser Comfig
+		set "uldr.iniParser=%uldr.appPath%bin\iniparser.exe"
+    	set "uldr.iniFile=%uldr.appPath%uniloader.ini"
+
+	:: Minimize / Restore
+		set "uldr.minResExe=%uldr.appPath%bin\minres.exe"
+
 	:: Splash Config		
-		set "uldr.splashExe=%uldr.appPath%bin\splash2.exe"		
+		set "uldr.splashExe=%uldr.appPath%bin\splash2.exe"
 		set "uldr.splashIPCFile=splash.IPC"
 
 	:: -- Browser Launch Config !! do not alter quoting !!
 		set uldr.browserPath="C:\Program Files\BraveSoftware\Brave-Browser\Application\chrome_proxy.exe"
 
 	:: -- Initialize Function Return Values
-		set checkServerRunning.Return=False
-	%RTS%
-}
-
-:Init_ComfyUI3 {
-	:: --- Web Server Config
-		set "uldr.port=8188"
-		set "uldr.ip=127.0.0.1"
-
-	:: -- Web App Config
-		set "uldr.appName=ComfyUI_3"
-		set "uldr.path=L:\comfy_ui_3"
-		set "uldr.loaderScript=%uldr.path%\_start.bat"
-
-	:: -- Splash Config
-		set "uldr.splashImage=%uldr.appPath%img\comfyui-02-splash.jpeg"
-	
-	:: --- Browser Launch Config ---
-		set "uldr.browserArgs=--profile-directory=Default --app-id=fdfllangmneamcbmopcnemepphpfaihc"
-
-	%RTS%
-}
-
-:Init_SillyTavern {
-	:: -- Web Server Config
-		set "uldr.port=8000"
-		set "uldr.ip=127.0.0.1"
-
-	:: -- Web App Config
-		set "uldr.appName=SillyTavern"
-		set "uldr.path=L:\SillyTavern"
-		set "uldr.loaderScript=%uldr.path%\Start.bat"
-
-	:: -- Splash Config
-		set "uldr.splashImage=%uldr.appPath%img\tavern-splash-05.jpg"
-
-	:: -- Browser Launch Config
-		set "uldr.browserArgs=--profile-directory=Default --app-id=mngloiodpbedloingimookgdhhkgcblo"
-
+		set CheckHTTPServerUP.Return=False
 	%RTS%
 }
 
@@ -116,25 +75,37 @@
 
 	%RTS%
 
-	:InitMessageTable.LowLevel
-		:: Don't embed variables inside LowLevel error strings! Init sequence will prevent them being active.
-		set "uldr.error[0000]=[FATAL ] Error: Valid Application name must be supplied in the shortcut."
-		set "uldr.error[0001]=[FAILED] Timed out waiting for the Application server to start."
-
-		set "uldr.warn[0000]=[ Warn ] invalid Splash State."
-		set "uldr.warn[0001]=[ Warn ] invalid MinMax State."
-		%RTS%
-
-	:InitMessageTable.Main
-		set "uldr.msg[0000]=Universal ML Loader (C) 2026 Autumn"
-		set "uldr.msg[0001]=[  OK  ] %uldr.AppName% server is running!"
-		set "uldr.msg[0002]=[ INFO ] Starting %uldr.AppName%..."
-		set "uldr.msg[0003]=[ INFO ] Launching %uldr.AppName% browser app..."
-		set "uldr.msg[0004]=[ INFO ] Browser opening disabled. Server available at: http://%uldr.IP%:%uldr.Port%"
-		set "uldr.msg[0005]=[ INFO ] Waiting for %uldr.AppName% server..."		
+	:: Don't embed variables inside LowLevel error strings! Init sequence will prevent them being active.
+	:InitMessageTable.LowLevel		
+		:: Messages
+		set "uldr.msg[0000]=Universal ML/AI Application Loader (C) 2026 Autumn"
 		set "uldr.msg[0006]=[ INFO ] Starting Background Splash Application"
 		set "uldr.msg[0007]=[ INFO ] Display Splash Screen"
 		set "uldr.msg[0008]=[ INFO ] Shutdown Splash Application"
+		set "uldr.msg[0009]=[ INFO ] Loading User Config..."
+		set "uldr.msg[0010]=[ INFO ] Loading External App Config..."
+		:: Fatal
+		set "uldr.error[0000]=[FATAL ] Error: Valid Application name must be supplied in the shortcut."
+		set "uldr.error[0001]=[FAILED] Timed out waiting for the Application server to start."
+		set "uldr.error[0002]=[FATAL ] Invalid arguments passed to ini Parser."
+		set "uldr.error[0003]=[FATAL ] Ini file was not found."
+		set "uldr.error[0004]=[FATAL ] Ini file Section not found."
+		set "uldr.error[0005]=[FATAL ] Ini file Key not found."
+		set "uldr.error[0006]=[FATAL ] Ini file Parser not found."
+
+		:: Warnings
+		set "uldr.warn[0000]=[ WARN ] invalid Splash State."
+		set "uldr.warn[0001]=[ WARN ] invalid Terminal State."
+
+		%RTS%
+
+	:InitMessageTable.Main
+		:: Messages		
+		set "uldr.msg[0001]=[  OK  ] %uldr.AppName% server is Up!"
+		set "uldr.msg[0002]=[ INFO ] Starting %uldr.AppName%..."
+		set "uldr.msg[0003]=[ INFO ] Launching %uldr.AppName% browser app..."
+		set "uldr.msg[0004]=[ INFO ] Browser opening disabled. Server available at: http://%uldr.IP%:%uldr.Port%"
+		set "uldr.msg[0005]=[ INFO ] Waiting for %uldr.AppName% server..."
 
 	%RTS%
 }
@@ -145,9 +116,14 @@
 	timeout /t %1 /nobreak > nul
 	%RTS%
 }
-
+ 
 :PauseApp {
 	pause
+	%RTS%
+}
+
+:ShowWelcomeMessage {	
+	%JSR% :DisplayMessage "%uldr.msg[0000]% %uldr.appver%" NL_Post
 	%RTS%
 }
 
@@ -230,7 +206,7 @@
 		%BEQ% :UpdateSplash.Quit
 
 	:: Exception: Fall-through
-	%JSR% :DisplayMessage "%uldr.warn[0000]%"	
+	%JSR% :DisplayMessage "%uldr.warn[0000]%"
 	%RTS%
 
 	:UpdateSplash.Init		
@@ -238,8 +214,8 @@
 		%RTS%
 
 	:UpdateSplash.Load
-		%JSR% :DisplayMessage "%uldr.msg[0006]%"
-		start /b %uldr.splashExe% %uldr.splashImage% %uldr.splashIPCFile%
+		%JSR% :DisplayMessage "%uldr.msg[0006]%"		
+		%JFR% /b %uldr.splashExe% %uldr.splashImage% %uldr.splashIPCFile%
 		set "uldr.splash.State=load.Done"
 		%RTS%
 
@@ -252,52 +228,48 @@
 	:UpdateSplash.Quit
 		%JSR% :DisplayMessage "%uldr.msg[0008]%"
 		echo quit > %uldr.splashIPCFile%
+		set "uldr.splash.State=null"
 		%RTS%
 }
 
-:MinMaxTerminal {
+:ControlTerminal {
 	:: Is Disbled?, exit if so
-	%CMP% "%uldr.minMaxTerminal.state%" "Disabled"
-		%BEQ% :MinMaxTerminal.Exit
+	%CMP% "%uldr.ControlTerminal.state%" "Disabled"
+		%BEQ% :ControlTerminal.Exit
 
 	:: State initialization check, initialize state variable or jump to toggles
 	%CMP% "%1" "Init"
-		%BEQ% :MinMaxTerminal.Init
+		%BEQ% :ControlTerminal.Init
 
-	:: State check : Min
-	%CMP% "%uldr.minMaxTerminal.state%" "Minimized"
-		%BEQ% :MinMaxTerminal.Maximize
+	:: State check : Minimized
+	%CMP% "%uldr.ControlTerminal.state%" "Minimized"
+		%BEQ% :ControlTerminal.Restore
 
-	:: State check : Max
-	%CMP% "%uldr.minMaxTerminal.state%" "Maximized"
-		%BEQ% :MinMaxTerminal.Minimize
+	:: State check : Restored
+	%CMP% "%uldr.ControlTerminal.state%" "Restored"
+		%BEQ% :ControlTerminal.Minimize
 
-	:: MinMaxTerminal Falltrhough State Safety and warn message
+	:: ControlTerminal Falltrhough State Safety and warn message
 	%JSR% :DisplayMessage "%uldr.warn[0001]%"
 	%RTS%
 
-	:MinMaxTerminal.Init
-		set "uldr.minMaxTerminal.state=Maximized"
+	:ControlTerminal.Init
+		set "uldr.controlTerminal.state=Restored"
 		%RTS%
 
-	:MinMaxTerminal.Minimize
+	:ControlTerminal.Minimize
 		:: Don't vanish in an instant like suspicious software.
-		%JSR% :Delay 2
-		powershell -Command "$api = Add-Type -MemberDefinition '[DllImport(\"user32.dll\")] public static extern IntPtr GetForegroundWindow(); [DllImport(\"user32.dll\")] public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);' -Name 'Win32Active' -PassThru; $api::ShowWindow($api::GetForegroundWindow(), 6)" > nul 2>&1
-		set "uldr.minMaxTerminal.state=Minimized"
-		%BRA% :MinMaxTerminal.Exit
+		%JSR% :Delay 2		
+		%JSR% %uldr.minResExe% Minimize
+		set "uldr.controlTerminal.state=Minimized"
+		%BRA% :ControlTerminal.Exit
 
-	:MinMaxTerminal.Maximize
-		powershell -Command "$api = Add-Type -MemberDefinition '[DllImport(\"user32.dll\")] public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);' -Name 'Win32Restore' -PassThru; Get-Process -Name WindowsTerminal, WindowsTerminalPreview -ErrorAction SilentlyContinue | Where-Object { $_.MainWindowHandle -ne 0 } | ForEach-Object { $api::ShowWindow($_.MainWindowHandle, 9) }" > nul 2>&1
-		set "uldr.minMaxTerminal.state=Maximized"
-		%BRA% :MinMaxTerminal.Exit
+	:ControlTerminal.Restore		
+		%JSR% %uldr.minResExe% Restore
+		set "uldr.controlTerminal.state=Restored"
+		%BRA% :ControlTerminal.Exit
 
-	:MinMaxTerminal.Exit
-	%RTS%
-}
-
-:ShowWelcomeMessage {	
-	%JSR% :DisplayMessage "%uldr.msg[0000]% %uldr.appver%" NL_Post
+	:ControlTerminal.Exit
 	%RTS%
 }
 
@@ -312,7 +284,7 @@
 	%RTS%
 }
 
-:CheckServerRunning {
+:CheckHTTPServerUP {
 	%JSR% :DisplayMessage "%uldr.msg[0005]%"
 
 	:: safely store flags for compare
@@ -321,45 +293,45 @@
 	:: Set X Register (XR) to 0 (loop counter)
 	%LDX% 0
 
-	:CheckServerRunning.Loop
-		:: HTTP Server check via powershell
-		powershell -command "try { $response = Invoke-WebRequest -Uri http://%uldr.IP%:%uldr.Port% -Method HEAD -TimeoutSec 1 -ErrorAction Stop -UseBasicParsing; if ($response.StatusCode -eq 200) { exit 0 } else { exit 1 } } catch { exit 1 }"
+	:CheckHTTPServerUP.Loop
+		:: HTTP Server check		
+		curl -s -o nul -I -f --max-time 1 http://%uldr.IP%:%uldr.Port%
 
-		:: Check if powershell returned an error while probing the http server.
+		:: Check if curl returned an error while probing the http server.
 		%CMP% %errorlevel% 0
-			%BEQ% :CheckServerRunning.IsRunning_True
-			%BNE% :CheckServerRunning.IsRunning_False
+			%BEQ% :CheckHTTPServerUP.True
+			%BNE% :CheckHTTPServerUP.False
 
-		:CheckServerRunning.IsRunning_True
+		:CheckHTTPServerUP.True
 			:: Run message
 			%JSR% :DisplayMessage "%uldr.msg[0001]%" NL_Pre
-			set checkServerRunning.Return=True
+			set CheckHTTPServerUP.Return=True
 
 			:: Branch to exit and restore flags
-			%BRA% :CheckServerRunning.Exit
+			%BRA% :CheckHTTPServerUP.Exit
 
-		:CheckServerRunning.IsRunning_False
+		:CheckHTTPServerUP.False
 			:: Increment loop counter
 			%INX%
-			set checkServerRunning.Return=False
+			set CheckHTTPServerUP.Return=False
 
 			:: Check for max retries, show timeout message on max
 			%CMP% %XR% %uldr.maxTriesHTTP%
-				%BEQ% :CheckServerRunning.Timeout
-				%BNE% :CheckServerRunning.Continue
+				%BEQ% :CheckHTTPServerUP.Timeout
+				%BNE% :CheckHTTPServerUP.Continue
 
-		:CheckServerRunning.Timeout
+		:CheckHTTPServerUP.Timeout
 			:: Output timout error
 			%JSR% :DisplayMessage "%uldr.error[0001]%" NL_Pre
-			%BRA% :CheckServerRunning.Exit
+			%BRA% :CheckHTTPServerUP.Exit
 
-		:CheckServerRunning.Continue
+		:CheckHTTPServerUP.Continue
 			echo|set /p="."
 			%JSR% :delay 1
 
-	%BRA% :CheckServerRunning.Loop
+	%BRA% :CheckHTTPServerUP.Loop
 
-	:CheckServerRunning.Exit
+	:CheckHTTPServerUP.Exit
 		:: Resstore flags after compare
 		%POPF%
 	%RTS%
@@ -371,9 +343,9 @@
 		%BNE% :LaunchBrowser.Try
 		%BEQ% :LaunchBrowser.Skip
 
-	:: Try to launch if server is running.
+	:: Try to launch if server is Up.
 	:LaunchBrowser.Try
-		%CMP% %checkServerRunning.Return% True
+		%CMP% %CheckHTTPServerUP.Return% True
 			%BNE% :LaunchBrowser.Exit
 
 		:: Launch Message		
@@ -394,29 +366,132 @@
 	%RTS%
 }
 
-:LoadAIAppVariables {
-	:: Compare command line argument to lauch commands
-	%CMP% "%1" "%uldr.app[0]%"
-		%BEQ% :LoadAIAppVariables.ComfyUI_3
+:ReadIniKV.ErrorHandler {
+	set "uldr.iniError=%1"
 
-	%CMP% "%1" "%uldr.app[1]%"
-		%BEQ% :LoadAIAppVariables.SillyTavern
+	%CMP% %uldr.iniError% 0
+		%BEQ% :ReadIniKV.ErrorHandler.OK
 
-	:: Fatal Exception Fall-through: Exit if no valid application name passed. Otherwise this is Branched over.
-	%JSR% :DisplayMessage "%uldr.error[0000]%" NL_Post
-	pause
-	%BRK% 1
+	%CMP% %uldr.iniError% 1
+		%BEQ% :ReadIniKV.ErrorHandler.Arg
 
-	:: Load specific variables for each application
-	:LoadAIAppVariables.ComfyUI_3
-		%JSR% :Init_ComfyUI3
-		%BRA% :LoadAIAppVariables.Exit
+	%CMP% %uldr.iniError% 2
+		%BEQ% :ReadIniKV.ErrorHandler.File
 
-	:LoadAIAppVariables.SillyTavern
-		%JSR% :Init_SillyTavern
-		%BRA% :LoadAIAppVariables.Exit
+	%CMP% %uldr.iniError% 3
+		%BEQ% :ReadIniKV.ErrorHandler.Section
 
-	:LoadAIAppVariables.Exit
+	%CMP% %uldr.iniError% 4
+		%BEQ% :ReadIniKV.ErrorHandler.Key
+
+	%RTS%
+
+	:ReadIniKV.ErrorHandler.OK
+        set /p uldr.iniResult=<"%TEMP%\uldr_ini_output.tmp"
+		%BRA% :ReadIniKV.ErrorHandler.Exit
+
+	:ReadIniKV.ErrorHandler.Arg
+		%JSR% :DisplayMessage "%uldr.error[0002]%" NL_Post
+		%BRA% :ReadIniKV.ErrorHandler.Break
+
+	:ReadIniKV.ErrorHandler.File
+		%JSR% :DisplayMessage "%uldr.error[0003]%" NL_Post
+		%BRA% :ReadIniKV.ErrorHandler.Break
+
+	:ReadIniKV.ErrorHandler.Section
+		%JSR% :DisplayMessage "%uldr.error[0004]%" NL_Post
+		%BRA% :ReadIniKV.ErrorHandler.Break
+
+	:ReadIniKV.ErrorHandler.Key
+		%JSR% :DisplayMessage "%uldr.error[0005]%" NL_Post
+		%BRA% :ReadIniKV.ErrorHandler.Break	
+
+	:ReadIniKV.ErrorHandler.Break
+		del "%TEMP%\uldr_ini_output.tmp" 2>nul
+		%JSR% :PauseApp
+		%BRK%
+
+	:ReadIniKV.ErrorHandler.Exit
+		del "%TEMP%\uldr_ini_output.tmp" 2>nul
+		%RTS%
+}
+
+:ReadIniKV {
+	:: Test if Ini FIle Exists
+	%FEX% "%uldr.iniParser%"
+		%BEQ% :ReadIniKV.Read
+		%BNE% :ReadIniKV.NoParser
+
+	:: Call external Parser with key and value, return error level, and pipe stdout (return value) to file.
+	:ReadIniKV.Read
+		:: Clear Result Before Qurey
+    	set "uldr.iniResult="
+    	%JSR% "%uldr.iniParser%" "%uldr.iniFile%" "%1" "%2" > "%TEMP%\uldr_ini_output.tmp"
+		%JSR% :ReadIniKV.ErrorHandler %errorlevel%
+		%BRA% :ReadIniKV.Exit
+
+	:ReadIniKV.NoParser
+		%JSR% :DisplayMessage "%uldr.error[0006]%"
+		%JSR% :PauseApp
+		%BRK% 6
+
+	:ReadIniKV.Exit
+    	%RTS%
+}
+
+:LoadUserConfig {
+	:: Loading
+	%JSR% :DisplayMessage "%uldr.msg[0009]%"
+
+	:: Delay before browser starts, allows user to read our console messages breifly.
+	%JSR% :ReadIniKV user browserDelay
+	set "uldr.browserDelay=%uldr.iniResult%"
+
+	:: Delay before exiting.
+	%JSR% :ReadIniKV user appDelay
+	set "uldr.appDelay=%uldr.iniResult%"
+
+	:: Number of retries to check if the HTTP Server is up.
+	%JSR% :ReadIniKV user maxTriesHTTP
+	set "uldr.maxTriesHTTP=%uldr.iniResult%"
+
+	:: Pauses at the end, and disables the Browser Launch.
+	%JSR% :ReadIniKV user debug
+	set "uldr.debug=%uldr.iniResult%"
+
+	:: options: Disabled / Void.
+	%JSR% :ReadIniKV user controlTerminal.state
+	set "uldr.controlTerminal.state=%uldr.iniResult%"
+	%RTS%
+}
+
+:LoadExternalAppConfig {
+	:: Loading Message
+	%JSR% :DisplayMessage "%uldr.msg[0010]%"
+
+	:: Web Server Config
+	%JSR% :ReadIniKV %1 AppName	
+	set "uldr.appName=%uldr.iniResult%"
+
+	%JSR% :ReadIniKV %1 port
+	set "uldr.port=%uldr.iniResult%"
+
+	%JSR% :ReadIniKV %1 ip
+	set "uldr.ip=%uldr.iniResult%"
+
+	%JSR% :ReadIniKV %1 path
+	set "uldr.path=%uldr.iniResult%"
+
+	%JSR% :ReadIniKV %1 loaderScript
+	set "uldr.loaderScript=%uldr.path%\%uldr.iniResult%"
+
+	:: Splash Config
+	%JSR% :ReadIniKV %1 splashImage
+	set "uldr.splashImage=%uldr.appPath%%uldr.iniResult%"
+	
+	%JSR% :ReadIniKV %1 browserArgs
+	set "uldr.browserArgs=%uldr.iniResult%"
+
 	%RTS%
 }
 
@@ -424,23 +499,25 @@
 
 :_main {
 	:: Initialization
+
 	%JSR% :InitNameSpace					:: Initialize this.application variable namespace.
 	%JSR% :InitMessageTable LowLevel		:: Initialize Low Level message strings.
-	%JSR% :LoadAIAppVariables %1			:: Load AI app specific variables from command line argument. ("Load" because maybe config file some day).
-	%JSR% :InitMessageTable Main			:: Initialize Main application message strings.
 	%JSR% :ShowWelcomeMessage
-	%JSR% :UpdateSplash	Init				:: Splash State machine Initialization.
-	%JSR% :MinMaxTerminal Init				:: MinMax state machine Initialization.
+	%JSR% :LoadExternalAppConfig %1			:: Load configuration variables for specific external HTTP Application.
+	%JSR% :InitMessageTable Main			:: Initialize Main application message strings.	
+	%JSR% :LoadUserConfig					:: Loads user configuration data from the ini file.
+	%JSR% :UpdateSplash	Init				:: Splash State Initialization.
+	%JSR% :ControlTerminal Init				:: Terminal state Initialization.
 
 	:: Body
 	%JSR% :UpdateSplash						:: Splash state transition: Loads splash image application in the background, waits for IPC commands.
 	%JSR% :StartHTTPServer					:: Start HTTP Server for AI Application.
 	%JSR% :UpdateSplash						:: Splash state transition: Sends Display IPC Command to the Splash Application.
-	%JSR% :MinMaxTerminal					:: MinMax Terminal State transition: Minimize Terminal.
-	%JSR% :CheckServerRunning				:: Check if server is running (loop with max).
+	%JSR% :ControlTerminal					:: Terminal State transition: Minimize Terminal.
+	%JSR% :CheckHTTPServerUP				:: Check if server is up (loop with max).
 
 	:: Exit
-	%JSR% :MinMaxTerminal					:: MinMax Terminal State transition: Maximize Terminal
+	%JSR% :ControlTerminal					:: Terminal State transition: Restore Terminal
 	%JSR% :UpdateSplash						:: Splash state transition: Sends Quit IPC Command to the Splash Application.
 	%JSR% :LaunchBrowser					:: Try to Launch Browser Application, Displays Browser IP/URL if Disabled in config.
 	%JSR% :Bye								:: Wave.
